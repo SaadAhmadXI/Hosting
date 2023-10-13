0730b2686e73a41caa15e48f591e941fc52e780a
Saad Ahmad <143178452+SaadAhmadXI@users.noreply.github.com>
2023-10-13 22:54:19 +0500

    Update script.js

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ document.addEventListener("DOMContentLoaded", function() {
         { name: "Elden Ring", thumbnail: "Elden Ring/thumbnail.jpg", url: "https://onelinkgames.site/Elden Ring" },
         { name: "FlatOut 4 Total Insanity", thumbnail: "FlatOut 4 Total Insanity/thumbnail.jpg", url: "https://onelinkgames.site/FlatOut 4 Total Insanity" },
         { name: "Vosphia", thumbnail: "Vosphia/thumbnail.jpg", url: "https://onelinkgames.site/Vosphia" },
-        { name: "Vosphia", thumbnail: "Vosphia/thumbnail.jpg", url: "https://onelinkgames.site/Vosphia" },
+        { name: "Hogwarts Legacy", thumbnail: "Hogwarts Legacy/thumbnail.jpg", url: "https://onelinkgames.site/Hogwarts Legacy" },
         { name: "Forza Horizan 5", thumbnail: "Forza Horizan 5/thumbnail.jpg", url: "https://onelinkgames.site/Forza Horizan 5" },
         // Add more subdirectories here
     ];
