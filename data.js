fe3a74e61b46c855f22e84201566336d5b406393
Saad Ahmad <143178452+SaadAhmadXI@users.noreply.github.com>
2023-10-16 09:56:51 +0500

    Update data.js

--- data.js
+++ data.js
@@ -1,8 +1,11 @@
 const allSubdirectories = [
-    { name: "FlatOut 4 Total Insanity", thumbnail: "../FlatOut 4 Total Insanity/thumbnail.jpg", url: "https://example.com/subdir1" },
-    { name: "Forza Horizan 5", thumbnail: "../Forza Horizan 5/thumbnail.jpg", url: "https://example.com/subdir2" },
-    { name: "Hogwarts Legacy", thumbnail: "Hogwarts Legacy/thumbnail.jpg", url: "https://example.com/subdir3" },
-    { name: "Vosphia", thumbnail: "Vosphia/thumbnail.jpg", url: "https://example.com/subdir4" },
+        { name: "Battlefield 1", thumbnail: "../Battlefield 1/thumbnail.jpg", url: "https://onelinkgames.site/Battlefield 1" },
+        { name: "Elden Ring", thumbnail: "../Elden Ring/thumbnail.jpg", url: "https://onelinkgames.site/Elden Ring" },
+        { name: "FlatOut 4 Total Insanity", thumbnail: "../FlatOut 4 Total Insanity/thumbnail.jpg", url: "https://onelinkgames.site/FlatOut 4 Total Insanity" },
+        { name: "Vosphia", thumbnail: "../Vosphia/thumbnail.jpg", url: "https://onelinkgames.site/Vosphia" },
+        { name: "Call of Duty Black Ops Cold War", thumbnail: "../Call of Duty Black Ops Cold War/thumbnail.jpg", url: "https://onelinkgames.site/Call of Duty Black Ops Cold War" },
+        { name: "Hogwarts Legacy", thumbnail: "../Hogwarts Legacy/thumbnail.jpg", url: "https://onelinkgames.site/Hogwarts Legacy" },
+        { name: "Forza Horizan 5", thumbnail: "../Forza Horizan 5/thumbnail.jpg", url: "https://onelinkgames.site/Forza Horizan 5" },
 ];
 
 export { allSubdirectories };
