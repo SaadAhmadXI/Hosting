631b2a0630f8349279bdf7b77ac8253367fe9ec5
Saad Ahmad <143178452+SaadAhmadXI@users.noreply.github.com>
2023-10-16 09:54:50 +0500

    Add files via upload

---
+++ data.js
@@ -0,0 +1,8 @@
+const allSubdirectories = [
+    { name: "FlatOut 4 Total Insanity", thumbnail: "../FlatOut 4 Total Insanity/thumbnail.jpg", url: "https://example.com/subdir1" },
+    { name: "Forza Horizan 5", thumbnail: "../Forza Horizan 5/thumbnail.jpg", url: "https://example.com/subdir2" },
+    { name: "Hogwarts Legacy", thumbnail: "Hogwarts Legacy/thumbnail.jpg", url: "https://example.com/subdir3" },
+    { name: "Vosphia", thumbnail: "Vosphia/thumbnail.jpg", url: "https://example.com/subdir4" },
+];
+
+export { allSubdirectories };
