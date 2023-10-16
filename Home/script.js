9e1b84a21beabf0257e46d786824b39c4492f8d4
Saad Ahmad <143178452+SaadAhmadXI@users.noreply.github.com>
2023-10-16 10:01:22 +0500

    Add files via upload

---
+++ Home/script.js
@@ -0,0 +1,55 @@
+import { allSubdirectories } from '../data.js';
+
+const pageSubdirectories = allSubdirectories.slice(0, 9);
+
+document.addEventListener("DOMContentLoaded", function() {
+    const thumbnailContainer = document.querySelector(".body");
+
+    function displaySubdirectories(subdirectories) {
+        thumbnailContainer.innerHTML = "";
+
+        subdirectories.forEach(subdirectory => {
+            const thumbnailBox = createThumbnailBox(subdirectory.name, subdirectory.thumbnail, subdirectory.url);
+            thumbnailContainer.appendChild(thumbnailBox);
+        });
+    }
+
+    function createThumbnailBox(name, thumbnail, url) {
+        const thumbnailBox = document.createElement("div");
+        thumbnailBox.className = "box";
+
+        const thumbnailLink = document.createElement("a");
+        thumbnailLink.href = url;
+        thumbnailLink.target = "_blank";
+        thumbnailLink.rel = "noopener noreferrer";
+
+        const thumbnailImage = document.createElement("img");
+        thumbnailImage.src = thumbnail;
+        thumbnailImage.alt = "Thumbnail";
+
+        const thumbnailTitle = document.createElement("p");
+        thumbnailTitle.className = "name";
+        thumbnailTitle.textContent = name;
+
+        thumbnailLink.appendChild(thumbnailImage);
+        thumbnailLink.appendChild(thumbnailTitle);
+        thumbnailBox.appendChild(thumbnailLink);
+
+        return thumbnailBox;
+    }
+
+    const searchInput = document.getElementById("searchInput");
+    searchInput.addEventListener("input", function() {
+        const filterText = searchInput.value;
+        if (filterText === "") {
+            // If the search input is cleared, show all subdirectories again
+            displaySubdirectories(pageSubdirectories);
+        } else {
+            const filteredSubdirectories = allSubdirectories.filter(subdirectory => subdirectory.name.toLowerCase().includes(filterText.toLowerCase()));
+            displaySubdirectories(filteredSubdirectories);
+        }
+    });
+
+    // Initial load of thumbnails
+    displaySubdirectories(pageSubdirectories);
+});
