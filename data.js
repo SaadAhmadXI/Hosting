7759e09b9f50bb75b7d3f4d7110bad2cc8bf3e4e
Saad Ahmad <143178452+SaadAhmadXI@users.noreply.github.com>
2023-10-19 12:10:26 +0500

    Update data.js

--- data.js
+++ data.js
@@ -1,13 +1,13 @@
 const allSubdirectories = [
         { name: "Battlefield 1", thumbnail: "../Battlefield 1/thumbnail.jpg", url: "https://onelinkgames.site/Battlefield 1" },
         { name: "Elden Ring", thumbnail: "../Elden Ring/thumbnail.jpg", url: "https://onelinkgames.site/Elden Ring" },
-        { name: "Cyberpunk 2077", thumbnail: "../Cyberpunk 2077/thumbnail.jpg", url: "https://onelinkgames.site/Cyberpunk 2077" },
         { name: "Call of Duty Black Ops Cold War", thumbnail: "../Call of Duty Black Ops Cold War/thumbnail.jpg", url: "https://onelinkgames.site/Call of Duty Black Ops Cold War" },
         { name: "Hogwarts Legacy", thumbnail: "../Hogwarts Legacy/thumbnail.jpg", url: "https://onelinkgames.site/Hogwarts Legacy" },
         { name: "Forza Horizan 5", thumbnail: "../Forza Horizan 5/thumbnail.jpg", url: "https://onelinkgames.site/Forza Horizan 5" },
         { name: "Red Dead Redemption 2", thumbnail: "../Red Dead Redemption 2/thumbnail.jpg", url: "https://onelinkgames.site/Red Dead Redemption 2" },
         { name: "FlatOut 4 Total Insanity", thumbnail: "../FlatOut 4 Total Insanity/thumbnail.jpg", url: "https://onelinkgames.site/FlatOut 4 Total Insanity" },
         { name: "Vosphia", thumbnail: "../Vosphia/thumbnail.jpg", url: "https://onelinkgames.site/Vosphia" },
+        { name: "Cyberpunk 2077", thumbnail: "../Cyberpunk 2077/thumbnail.jpg", url: "https://onelinkgames.site/Cyberpunk 2077" },
 ];
 
 export { allSubdirectories };
