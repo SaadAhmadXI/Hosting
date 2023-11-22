793c8025e811f0a4fd5022f6cd3a46c28bdc1f63
Saad Ahmad <xinstinctgaming123@gmail.com>
2023-11-22 16:20:34 +0500

    saad

--- data.js
+++ data.js
@@ -1,11 +1,12 @@
 const allSubdirectories = [
+        { name: "NARUTO X BORUTO Ultimate Ninja STORM CONNECTIONS", thumbnail: "../NARUTO X BORUTO Ultimate Ninja STORM CONNECTIONS/thumbnail.jpg", url: "https://onelinkgames.site/NARUTO X BORUTO Ultimate Ninja STORM CONNECTIONS" },
         { name: "Elden Ring", thumbnail: "../Elden Ring/thumbnail.jpg", url: "https://onelinkgames.site/Elden Ring" },
         { name: "Cyberpunk 2077", thumbnail: "../Cyberpunk 2077/thumbnail.jpg", url: "https://onelinkgames.site/Cyberpunk 2077" },
         { name: "Hogwarts Legacy", thumbnail: "../Hogwarts Legacy/thumbnail.jpg", url: "https://onelinkgames.site/Hogwarts Legacy" },
         { name: "Horizon Zero Dawn", thumbnail: "../Horizon Zero Dawn/thumbnail.jpg", url: "https://onelinkgames.site/Horizon Zero Dawn" },
-        { name: "Call of Duty Black Ops Cold War", thumbnail: "../Call of Duty Black Ops Cold War/thumbnail.jpg", url: "https://onelinkgames.site/Call of Duty Black Ops Cold War" },
         { name: "Forza Horizon 5", thumbnail: "../Forza Horizon 5/thumbnail.jpg", url: "https://onelinkgames.site/Forza Horizon 5" },
         { name: "Red Dead Redemption 2", thumbnail: "../Red Dead Redemption 2/thumbnail.jpg", url: "https://onelinkgames.site/Red Dead Redemption 2" },
+        { name: "Call of Duty Black Ops Cold War", thumbnail: "../Call of Duty Black Ops Cold War/thumbnail.jpg", url: "https://onelinkgames.site/Call of Duty Black Ops Cold War" },
         { name: "Alan Wake 2", thumbnail: "../Alan Wake 2/thumbnail.jpg", url: "https://onelinkgames.site/Alan Wake 2" },
         { name: "Vosphia", thumbnail: "../Vosphia/thumbnail.jpg", url: "https://onelinkgames.site/Vosphia" },
         { name: "Battlefield 1", thumbnail: "../Battlefield 1/thumbnail.jpg", url: "https://onelinkgames.site/Battlefield 1" },
