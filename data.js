30ec1dc059e99273bf52482bfe74ad400011d32c
Saad Ahmad <xinstinctgaming123@gmail.com>
2023-11-25 19:18:49 +0500

    saad

--- data.js
+++ data.js
@@ -6,12 +6,14 @@ const allSubdirectories = [
         { name: "Horizon Zero Dawn", thumbnail: "../Horizon Zero Dawn/thumbnail.jpg", url: "https://onelinkgames.site/Horizon Zero Dawn" },
         { name: "Forza Horizon 5", thumbnail: "../Forza Horizon 5/thumbnail.jpg", url: "https://onelinkgames.site/Forza Horizon 5" },
         { name: "Red Dead Redemption 2", thumbnail: "../Red Dead Redemption 2/thumbnail.jpg", url: "https://onelinkgames.site/Red Dead Redemption 2" },
+        { name: "MARVEL’S SPIDER-MAN MILES MORALES", thumbnail: "../MARVEL’S SPIDER-MAN MILES MORALES/thumbnail.jpg", url: "https://onelinkgames.site/MARVEL’S SPIDER-MAN MILES MORALES" },
         { name: "Call of Duty Black Ops Cold War", thumbnail: "../Call of Duty Black Ops Cold War/thumbnail.jpg", url: "https://onelinkgames.site/Call of Duty Black Ops Cold War" },
         { name: "Alan Wake 2", thumbnail: "../Alan Wake 2/thumbnail.jpg", url: "https://onelinkgames.site/Alan Wake 2" },
         { name: "Vosphia", thumbnail: "../Vosphia/thumbnail.jpg", url: "https://onelinkgames.site/Vosphia" },
         { name: "Battlefield 1", thumbnail: "../Battlefield 1/thumbnail.jpg", url: "https://onelinkgames.site/Battlefield 1" },
         { name: "FlatOut 4 Total Insanity", thumbnail: "../FlatOut 4 Total Insanity/thumbnail.jpg", url: "https://onelinkgames.site/FlatOut 4 Total Insanity" },
         { name: "Sekiro Shadow Die Twice", thumbnail: "../Sekiro Shadow Die Twice/thumbnail.jpg", url: "https://onelinkgames.site/Sekiro Shadow Die Twice" },
+        { name: "Gears of War Ultimate Edition Deluxe", thumbnail: "../Gears of War Ultimate Edition Deluxe/thumbnail.jpg", url: "https://onelinkgames.site/Gears of War Ultimate Edition Deluxe" },
 ];
 
 export { allSubdirectories };
